7742b8383007f83b7b539a215d1b9677a2e1558c
younes El bettate <bt.younesse@gmail.com>
2025-08-04 20:34:27 +0100

    fix base page for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/",
+  base: isGitHubPages ? '/ksar-data/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
